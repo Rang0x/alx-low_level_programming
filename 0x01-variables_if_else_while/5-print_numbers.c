3a1d218a6e27651ead22502e2a227290cc610d83
Abdelrahman Emad <abdelrahmanaemad@gmail.com>
2023-06-19 16:50:30 -0700

    numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : printnumbers
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		printf("%i", number);
+	}
+	printf("\n")
+
+	return (0);
+}
