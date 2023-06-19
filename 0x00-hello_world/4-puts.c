64128bf7cdce6f08aeb5a2273a0a0913696248a8
Abdelrahman Emad <abdelrahmanaemad@gmail.com>
2023-06-19 06:50:07 -0700

    putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main function >> Entry point
- * * This is hello world program using c
- * Return equal 0
+ * Description:  This is hello world program using c
+ * Return:  equal 0
 */
 int main(void)
 {
