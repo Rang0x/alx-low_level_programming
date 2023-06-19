288d7f81520fa370e540a47b0c31b78d32e50a77
Abdelrahman Emad <abdelrahmanaemad@gmail.com>
2023-06-19 06:48:42 -0700

    putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main function >> Entry point 
- * This is hello world program using c
+ * main function >> Entry point
+ * * This is hello world program using c
  * Return equal 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
