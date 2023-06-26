8bda10756c70b51c132454ceab9dc4e81fde3a4f
Abdelrahman Emad <abdelrahmanaemad@gmail.com>
2023-06-26 08:42:18 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints lowercase alphabet using prototype _putchar
+ * Return: Always 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
+
